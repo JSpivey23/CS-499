09e97fea05ba8e088ebd65c187b17e67ae054c9c
Jim Spivey <jspiveyjr@gmail.com>
2025-09-05 14:36:57 -0400

    inital commit

---
+++ app_admin/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, HttpClientModule } from '@angular/common/http';
+
+import { routes } from './app.routes';
+import {authInterceptProvider} from './utils/jwt-interceptor';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+   provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(HttpClientModule),
+    authInterceptProvider
+  ]
+};
---
+++ app_admin/src/app/trip-card/trip-card.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Trip } from '../models/trip';
+import { Authentication } from '../services/authentication';
+
+@Component({
+  selector: 'app-trip-card',
+  imports: [CommonModule],
+  templateUrl: './trip-card.html',
+  styleUrl: './trip-card.css',
+})
+export class TripCard implements OnInit {
+  @Input('trip') trip: any;
+
+  constructor(
+private router: Router,
+private authenticationService: Authentication
+) {}
+
+public isLoggedIn()
+{
+return this.authenticationService.isLoggedIn();
+}
+
+  ngOnInit(): void {}
+
+  public editTrip(trip: Trip) {
+    localStorage.removeItem('tripCode');
+    localStorage.setItem('tripCode', trip.code);
+    this.router.navigate(['edit-trip']);
+  }
+}
